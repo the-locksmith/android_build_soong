df2b4af9a1c0b2fcf98ecd7612d42d992d52ed6f
Simon Shields <simon@lineageos.org>
2017-08-23 12:05:33 +1000

    soong: extend with octavi board flags
    Includes the following commit:
    
    Author: Simon Shields <simon@lineageos.org>
    Date:   Mon Sep 4 21:09:44 2017 +1000
    
        soong: explicitly specify name of lucid variables struct
    
        Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"octavi/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -124,6 +126,8 @@ type variableProperties struct {
 			Static_libs  []string
 			Srcs         []string
 		}
+		// include Octavi variables
+		Octavi android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -287,6 +291,9 @@ type productVariables struct {
 	ProductHiddenAPIStubsTest   []string `json:",omitempty"`
 
 	TargetFSConfigGen []string `json:",omitempty"`
+
+	// include Octavi variables
+	Octavi android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
